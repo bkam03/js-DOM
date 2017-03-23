8c4ca9874192a7d3f6b191e73df9246a07c54612
bkam03 <bkam03@yahoo.com>
2017-03-23 13:55:53 -1000

    ex 5

--- js/app.js
+++ js/app.js
@@ -32,7 +32,8 @@ snoopTalks[0].innerHTML = "Lorizzle i'm in the shizzle shiznit sit things, pimpi
 
 /*5. Relace the n/a for Samuel L Jackson with his lorem ipsum*/
 
-
+var jacksonSays = document.getElementsByClassName("profile");
+jacksonSays[2].innerHTML = "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.";
 
 
 /*6. Create a div element for Chuck Norris and give it an id of "name7". Inside this div element, give it the contents of "Chuck Norris".
